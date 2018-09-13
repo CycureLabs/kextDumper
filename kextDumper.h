44ef3529b8d5a1bda11db064395fb678f5e07a12
Rajesh Majumdar <rajeshmjmdr02@gmail.com>
2018-09-13 23:18:43 +0530

    Create kextDumper.h

---
+++ kextDumper.h
@@ -0,0 +1,13 @@
+//
+//  kextdumper.h
+//
+//  Created by Cycure Labs on 13/09/18.
+//
+
+#ifndef kextdumper_h
+#define kextdumper_h
+
+#include <stdio.h>
+#import <Foundation/Foundation.h>
+int kext_dumper(NSString* path);
+#endif /* kextdumper_h */
